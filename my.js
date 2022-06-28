e1191b16977361d01f9ffa435187eed8b0c0b545
CodeStarPrit <97210137+CodeStarPrit@users.noreply.github.com>
2022-06-28 17:48:28 +0530

    initial

---
+++ my.js
@@ -0,0 +1,21 @@
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+
+// window.onscroll = () => {
+
+//     if (window.scrollY > 100) {
+//         document.querySelector('.myheader').classList.add('active');
+//     } else {
+//         document.querySelector('.myheader').classList.remove('active');
+//     }
+
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
+
